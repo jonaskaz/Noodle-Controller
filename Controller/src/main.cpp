258ee85a5f5f84d2df90c1d46154fa435608c976
toluooshy <toluooshy@gmail.com>
2021-11-18 12:53:05 -0500

    Added button controls and AtMega functionality.

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -2,7 +2,7 @@
 #include <StepMotor.h>
 #include <MultiStepper.h>
 #include <ArduinoJson.h>
-#include <ButtonController.h>
+#include <ezButton.h>
 
 #define yMaxPin 10
 #define xMaxPin 6
@@ -14,55 +14,79 @@ const int dirPins[3] = {2, 4, 8};
 
 StepMotor step = StepMotor(motorInterfaceType, stepPins, dirPins);
 
-// Button buttonUp(10);
-// Button buttonDown(11);
-Button buttonLeft(12);
-Button buttonRight(13);
-// Button buttonStart(12);
-// Button buttonStop(13);
+ezButton buttonLeft(12);
+ezButton buttonRight(13);
 
-ButtonController controller;
+bool goingLeft = false;
+bool goingRight = false;
 
 void setup()
 {
     Serial.begin(112500);
+    buttonLeft.setDebounceTime(50);  // set debounce time to 50 milliseconds
+    buttonRight.setDebounceTime(50); // set debounce time to 50 milliseconds
     int startingPos[2] = {0, 0};
     step.setup(1000, 500, 1000, 1000, startingPos);
     delay(5000);
-    step.calibrate(yMaxPin, xMinPin, xMaxPin);
+    // step.calibrate(yMaxPin, xMinPin, xMaxPin);
 }
 
 void loop()
 {
-    // stepper.goTo(controller.horizontalButtonControl(buttonLeft, buttonRight));
+    buttonLeft.loop();
+    buttonRight.loop();
 
-    if (Serial.available() > 0)
+    if (buttonLeft.isPressed())
+        goingLeft = true;
+
+    if (buttonLeft.isReleased())
+        goingLeft = false;
+
+    if (buttonRight.isPressed())
+        goingRight = true;
+
+    if (buttonRight.isReleased())
+        goingRight = false;
+
+    if (goingLeft)
     {
-        int mode = Serial.parseInt();
-        String flavor = Serial.readStringUntil('\n');
-        String toppings = Serial.readStringUntil('\n');
-        StaticJsonDocument<200> doc;
-        deserializeJson(doc, toppings);
-        JsonArray toppingsArray = doc.as<JsonArray>();
-        switch (mode)
-        {
-        case 0:
-            break;
-        case 1:
-            break;
-        default:
-            break;
-        }
-        String beef = "beef";
-        flavor.trim();
-        if (flavor.equals(beef))
-        {
-            step.goToB(75, 75);
-            Serial.println(flavor);
-        }
-        else
-        {
-            ;
-        }
+        // Serial.println("L");
+        step.goTo(0, 0);
     }
+
+    if (goingRight)
+    {
+        // Serial.println("R");
+        step.goTo(100, 0);
+    }
+
+    // if (Serial.available() > 0)
+    // {
+    //     int mode = Serial.parseInt();
+    //     String flavor = Serial.readStringUntil('\n');
+    //     String toppings = Serial.readStringUntil('\n');
+    //     StaticJsonDocument<200> doc;
+    //     deserializeJson(doc, toppings);
+    //     JsonArray toppingsArray = doc.as<JsonArray>();
+    //     switch (mode)
+    //     {
+    //     case 0:
+    //         break;
+    //     case 1:
+    //         break;
+    //     default:
+    //         break;
+    //     }
+    //     String beef = "beef";
+    //     flavor.trim();
+    //     if (flavor.equals(beef))
+    //     {
+    //         step.goToB(75, 75);
+    //         Serial.println(flavor);
+    //     }
+    //     else
+    //     {
+    //         ;
+    //     }
+    // }
 }
